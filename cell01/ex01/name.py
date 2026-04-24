b1159c777b6cf369fc3227ecef974daef195458a
ZENLIK02 <zen.likh@gmail.com>
2026-04-24 07:33:41 +0000

    ugcuvvgvgv

---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "thanat"
+last_name = "likithrungrueng"
+whole_name = first_name,last_name
+
+print(whole_name)
---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+first_name = input("Hey, what's your first name? : ")
+last_name = input("And what is your lastname? : ")
+result = first_name + ' ' + last_name
+print(f"Well, pleased to meet you, {result}")
